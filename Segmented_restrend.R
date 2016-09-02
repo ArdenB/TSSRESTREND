d91e0f2021834d3900bbfda785335346dda6c83c
ArdenB <arden.burrell@student.unsw.edu.au>
2016-09-02 18:46:23 +1000

    test commit

--- Segmented_restrend.R
+++ Segmented_restrend.R
@@ -3,7 +3,10 @@
 library("RcppCNPy")
 #library("gap", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
 library("strucchange")
+
 setwd("/mnt/FCBE3028BE2FD9C2/Users/user/Documents/segres_demo")
+
+#load the data
 
 load("demo_data/stdRESTREND.Rda")
 load("demo_data/stdRESTREND_CTSR.Rda")
