67a2bd1de426e9d01e75e7a38fcfbbde88b3e932
ArdenB <arden.burrell@student.unsw.edu.au>
2016-09-02 18:34:05 +1000

    first commit

---
+++ Segmented_restrend.R
@@ -0,0 +1,9 @@
+#library("bfast")
+#library("forecast")
+library("RcppCNPy")
+#library("gap", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+library("strucchange")
+setwd("/mnt/FCBE3028BE2FD9C2/Users/user/Documents/segres_demo")
+
+load("demo_data/stdRESTREND.Rda")
+load("demo_data/stdRESTREND_CTSR.Rda")
