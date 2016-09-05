cea90ff4852ec4ddcb153136e9166181e2bc7b59
ArdenB <arden.burrell@student.unsw.edu.au>
2016-09-05 19:22:52 +1000

    new data point and segRES functionality added

--- Segmented_restrend.R
+++ Segmented_restrend.R
@@ -3,6 +3,7 @@ library("bfast")
 library("RcppCNPy")
 #library("gap", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
 library("strucchange")
+library("broom")
 
 setwd("/mnt/FCBE3028BE2FD9C2/Users/user/Documents/segres_demo")
 
@@ -10,6 +11,8 @@ setwd("/mnt/FCBE3028BE2FD9C2/Users/user/Documents/segres_demo")
 
 load("./demo_data/stdRESTREND.Rda")
 load("./demo_data/stdRESTREND_CTSR.Rda")
+load("./demo_data/segRESTREND.Rda")
+load("./demo_data/segRESTREND_CTSR.Rda")
 
 
 
@@ -61,9 +64,9 @@ BFAST.RESID <- function(CTSR.VI, CTSR.RF, print=FALSE, plot=FALSE) {
   }else {return(FALSE)}
   
 }
-# Missing the chow test
 
-RESTREND <- function(anu.VI, acu.RF, VI.index, sig=0.05, breakpoint=FALSE, print=FALSE, plot=FALSE) {
+CHOW <- function(anu.VI, acu.RF, VI.index, breakpoints, sig=0.05, print=FALSE){
+  #test the data to make sure its valid
   if (class(anu.VI) != "ts") 
     stop("anu.VI Not a time series object")
   if (class(acu.RF) != "ts") 
@@ -77,44 +80,289 @@ RESTREND <- function(anu.VI, acu.RF, VI.index, sig=0.05, breakpoint=FALSE, print
     stop("ts object do not have the same time")
   if (!identical(f, f2))
     stop("ts object do not have the same frequency")
-  if (!breakpoint){
-    VPR.fit <- lm(anu.VI ~ acu.RF)
-    #Convert to a ts object
-    #may wat to add a nonparametric trend test here
-    
-    
-    #Critical threshold test 
-    if (summary(VPR.fit)$coefficients[,4][2] > sig){
-      print("VPR significance below critical threshold")
-      return(structure(list(Method = FALSE, 
-                            VPR = VPR.fit, TSS.RESTREND = FALSE, total.change = FALSE), class = "RESTREND Object"))
-    } 
-    VPR.resid<- ts(VPR.fit$residuals, start=start(ti), end=end(ti), frequency = f)
-    RES <- lm(VPR.resid ~ ti)
-    if (plot){
-      plot(c(start(ti)[1]:end(ti)[1]), VPR.fit$residuals, pch=16,xlab="Accumulated Rainfall", 
-           ylab="Annual VI", col="orange",main="VI vs Precip")
-      abline(RES, col = "red",lwd = 2, lty = "dashed")# need to add a trend line 
+  # need to test the breakpoints to make sure they are numeric
+  if (class(breakpoints) != "numeric") 
+    stop("Breakpoints are not class numeric")
+  
+  #count of all the breakpoints
+  len <- length(breakpoints)
+  # convert the breakpoints into year posistion
+    #the breakpoint loc will be the last anaual max before the breakpoint
+  
+  #empty variables to add to a datafram
+  empty.1 <- NaN
+  empty.2 <- NaN 
+  empty.3 <- NaN
+  ind.df <- data.frame(abs.index=breakpoints, yr.index = empty.1, reg.sig=empty.2, VPR.bpsig = empty.3)
+  for (bp in 1:length(breakpoints)){
+    for (n in 1:length(VI.index)){
+      if (se>=VI.index[n] & se<=VI.index[n+1]){
+        #print(n)}
+        ind.df$yr.index[bp] = n
+        }
     }
-    init <- RES$fitted.values[1]
-    fin <- RES$fitted.values[end(RES$fitted.values)[1]]
-    change <- fin - init
-    return(structure(list(Method = "RESTREND", 
-                          VPR = VPR.fit, TSS.RESTREND = RES, total.change = change), class = "RESTREND Object"))
-    
-  } else{
-    print("segres")
+  }
+  #create the lm for the VPR and test is significance, 
+  #split VPR sig from VPR insignificant 
+  VPR.fit <- lm(anu.VI ~ acu.RF)
+  if (summary(VPR.fit)$coefficients[,4][2] > sig){
+    if (print){print("VPR significance below critical threshold, Testing breakpoints in the VPR")}
+    ind <- acu.RF #independent variable
+    dep <- anu.VI #dependent variable
+    Method = "seg.VPR"
+  } else {
+    ind <- ti
+    dep <- VPR.fit$residuals
+    Method = "seg.RESTREND"
+  } 
+  #Iterate over each of the breakpoints
+  while (TRUE){
+    for (bp.num in nrow(ind.df)){ #the breakpoints number, first bp is 1,  etc
+      bp = ind.df$yr.index[bp.num]
+      #start and ends
+      if (identical(ind.df$yr.index[bp.num-1], numeric(0))){
+        bp.start = 1
+        print("here0")
+      }else{
+        bp.start = ind.df$yr.index[bp.num-1]
+        print("here1")}
+      if (is.na(ind.df$yr.index[bp.num+1])){
+        bp.end = length(dep)
+      }else{
+        bp.end = ind.df$yr.index[bp.num+1]}
+      #perform the chow test
+      bkp = bp - (bp.start-1)
+      print(bkp)
+      chow <- sctest(dep[bp.start:bp.end] ~ ind[bp.start:bp.end], type = "Chow", point = bkp)
+
+      
+      ind.df$reg.sig[bp.num] = chow$p.value
+      
+    }
+    if (nrow(ind.df)>1){ #*****This needs to be tested with mutiple breakpoints*****
+      #delete breakpoint with the largest p values (lowest significance)
+      ind.df <- ind.df[!(1:nrow(ind.df) %in% (which.max(ind.df$reg.sig))),]
+    }else if (nrow(ind.df)==1){
+      if (print){
+        print(chow)
+      }
+      VPR.chow<- sctest(anu.VI ~ acu.RF, type = "Chow", point = ind.df$yr.index[1])
+      ind.df$VPR.bpsig[1] = VPR.chow$p.value
+      
+      if (Method == "seg.VPR" & ind.df$reg.sig[1] > sig){ # cant chow non-sig residulas (bpRESID.chow = FALSE)
+        ind.df$reg.sig[1] = NaN
+        return(structure(list(n.Method = FALSE, bp.summary = ind.df,
+                              bpRESID.chow = FALSE, bpVPR.chow=VPR.chow), class = "CHOW.Object"))
+      }else if (Method == "seg.VPR" & ind.df$reg.sig[1] <= sig){
+        ind.df$reg.sig[1] = NaN
+        return(structure(list(n.Method = "seg.VPR", bp.summary = ind.df,
+                              bpRESID.chow = FALSE, bpVPR.chow=VPR.chow), class = "CHOW.Object"))
+      }else if (Method == "seg.RESTREND" & ind.df$reg.sig[1] > sig){
+        return(structure(list(n.Method = "RESTREND", bp.summary = ind.df,
+                              bpRESID.chow = chow, bpVPR.chow=VPR.chow), class = "CHOW.Object"))
+      }else if (Method == "seg.RESTREND" & ind.df$reg.sig[1] <= sig){
+        if (VPR.chow$p.value>sig){
+          return(structure(list(n.Method = "seg.RESTREND", bp.summary = ind.df,
+                              bpRESID.chow = chow, bpVPR.chow=VPR.chow), class = "CHOW.Object"))
+        }else if(VPR.chow$p.value<=sig){
+          return(structure(list(n.Method = "seg.VPR", bp.summary = ind.df,
+                                bpRESID.chow = chow, bpVPR.chow=VPR.chow), class = "CHOW.Object"))
+        }
+      }else{
+        print("Error in Method and df, exit point 1")
+        return(FALSE)
+      }
+    }else{
+      print("ind.df shape is wrong, exited to avoid infinit loop, exit point 2")
+      return(FALSE)
+    }
   }
 }
+
+#final methods
+seg.RESTREND <- function(anu.VI, acu.RF, VI.index, breakpoint,  sig=0.05, print=FALSE, plot=FALSE){
+  if (class(anu.VI) != "ts") 
+    stop("anu.VI Not a time series object")
+  if (class(acu.RF) != "ts") 
+    stop("acu.VI Not a time series object")
+  ti <- time(anu.VI)
+  f <- frequency(anu.VI)
+  #check the two ts object cover the same time period
+  ti2 <- time(acu.RF)
+  f2 <- frequency(acu.RF)
+  if (!identical(ti, ti2))
+    stop("ts object do not have the same time range")
+  if (!identical(f, f2))
+    stop("ts object do not have the same frequency")
+  if (class(breakpoint) != "integer")
+    stop("Breakpoint must be an interger")
+  if (length(breakpoint) != 1)
+    stop("Breakpoint must be an interger of length 1")
+  
+  #Get the VPR
+  VPR.fit <- lm(anu.VI ~ acu.RF)
+  #Convert to a ts object
+  #may wat to add a nonparametric trend test here
+  
+  #Critical threshold test 
+  if (summary(VPR.fit)$coefficients[,4][2] > sig)
+    stop("VPR significance below critical threshold. Variables should not be passed to this function")
+  
+  VPR.resid<- ts(VPR.fit$residuals, start=start(ti), end=end(ti), frequency = f)
+  #Create the dummy variable
+  dummy <- rep(0, length(VPR.resid))
+  dummy[(breakpoint+1):length(VPR.resid)] = 1
+  
+  segRES.df = data.frame(year=ti, VPR.residuals=VPR.resid,  dummy.var=dummy)
+  
+  start = as.integer(start(ti)[1])
+  bkp = breakpoint + start-1
+  
+  bpanalysis<-lm(VPR.residuals~I(year-(bkp+0.5))*dummy.var,segRES.df)
+  if (print){
+    print(summary(bpanalysis))
+  }
+
+  if (plot){
+    len <- length(VPR.resid)
+    R.fit <- lm(VPR.residuals ~ year, segRES.df)
+    R.fit0 <- lm(VPR.residuals[1:breakpoint] ~ year[1:breakpoint], segRES.df)
+    R.fit1 <- lm(VPR.residuals[breakpoint+1:len] ~ year[breakpoint+1:len], segRES.df)
+    
+    start = as.integer(start(ti)[1])
+    end = as.integer(end(ti)[1])
+    xlim = c(start, end)
+    m.range = 2*max(abs(bpanalysis$fitted.values))
+    
+    RESchow <- sctest(segRES.df$VPR.residuals ~ segRES.df$year, type = "Chow", point = breakpoint)
+    plot(segRES.df$year[1:breakpoint], segRES.df$VPR.residuals[1:breakpoint], pch=16,
+         xlab="time", ylab="Residuals", col="orange", xlim=xlim, ylim = c(-m.range, m.range))
+    par(new=T)   
+    plot(segRES.df$year[breakpoint+1:len], segRES.df$VPR.residuals[breakpoint+1:len], pch=16,
+         xlab="", ylab="", col="purple", main="", 
+         xlim=c(start, end), ylim = c(-m.range, m.range))
+    abline(R.fit, col = "red",lwd = 2, lty = "dashed")
+    abline(v=(breakpoint-0.5+start), col="black", lwd = 2, lty = "dashed")
+    par(new=T)   
+    bpa.fitts <- ts(bpanalysis$fitted.values, start=ti[1], end=tail(ti, 1), frequency = f) 
+    b4.bp = bpanalysis$coefficients[[1]]
+    af.bp = bpanalysis$coefficients[[1]] + bpanalysis$coefficients[[3]]
+    bpats2 <- append(bpa.fitts, c(b4.bp, af.bp), after = breakpoint)
+    t2 <- append(ti, c(start+breakpoint-0.50001, start+breakpoint-0.49999), after=breakpoint)
+    plot(t2, bpats2, pch=16, type = "l", lwd = 2,
+         xlab="", ylab="", col="red", main="", 
+         xlim=c(start, end), ylim = c(-m.range, m.range))
+    
+    #Need to change the stastics that is shows here
+    
+    R.Fval = summary(bpanalysis)$f[[1]]
+    R.pval = glance(bpanalysis)$p.value
+    R.Rval = summary(bpanalysis)$r.squared
+    rp = vector('expression',3)
+    rp[1] = substitute(expression(italic(R^2) == R.Rval), 
+                       list(R.Rval = format(R.Rval,dig=3)))[2]
+    rp[2] = substitute(expression(italic(F) == R.Fval), 
+                       list(R.Fval  = format(R.Fval,dig=3)))[2]
+
+    rp[3] = substitute(expression(italic(p) == R.pval), 
+                       list(R.pval  = format(R.pval, digits = 3)))[2]
+    legend('topleft', legend = rp, bty = 'n')
+  }
+  
+  init <- bpanalysis$fitted.values[1]
+  fin <- bpanalysis$fitted.values[end(bpanalysis$fitted.values)[1]]
+  change <- as.numeric(fin - init)
+  return(structure(list(Method = "segmented.RESTREND", 
+                        VPR = VPR.fit, TSS.RESTREND = bpanalysis, total.change = change), class = "RESTREND.Object"))
+  
+}
+
+RESTREND <- function(anu.VI, acu.RF, VI.index, sig=0.05, print=FALSE, plot=FALSE) {
+  if (class(anu.VI) != "ts") 
+    stop("anu.VI Not a time series object")
+  if (class(acu.RF) != "ts") 
+    stop("acu.VI Not a time series object")
+  ti <- time(anu.VI)
+  f <- frequency(anu.VI)
+  #check the two ts object cover the same time period
+  ti2 <- time(acu.RF)
+  f2 <- frequency(acu.RF)
+  if (!identical(ti, ti2))
+    stop("ts object do not have the same time")
+  if (!identical(f, f2))
+    stop("ts object do not have the same frequency")
+  
+  #Get the VPR
+  VPR.fit <- lm(anu.VI ~ acu.RF)
+  #Convert to a ts object
+  #may wat to add a nonparametric trend test here
+  
+  #Critical threshold test 
+  if (summary(VPR.fit)$coefficients[,4][2] > sig){
+    if (print){ print("VPR significance below critical threshold")}
+    return(structure(list(Method = FALSE, 
+                          VPR = VPR.fit, TSS.RESTREND = FALSE, total.change = FALSE), class = "RESTREND.Object"))
+  } 
+  VPR.resid<- ts(VPR.fit$residuals, start=start(ti), end=end(ti), frequency = f)
+  RES <- lm(VPR.resid ~ ti)
+  
+  #!!!!!!!!!!!!!!!!!!!!!!!! Plots need to be tested and improved !!!!!!!!!!!!!!!!!!!!!!!
+  if (plot){
+    m.range = 2*max(abs(RES$fitted.values))
+    plot(c(start(ti)[1]:end(ti)[1]), VPR.fit$residuals, pch=16,xlab="Accumulated Rainfall", 
+         ylab="Annual VI", col="orange",main="VI vs Precip", ylim = c(-m.range, m.range))
+    par(new=T)   
+    plot(c(start(ti)[1]:end(ti)[1]), RES$fitted.values, type = "l", lwd = 2, pch=16,
+         xlab="", ylab="", col="red", main="", ylim = c(-m.range, m.range))
+    
+    R.Fval = summary(RES)$f[[1]]
+    R.Rval = summary(RES)$r.squared
+    R.pval = glance(RES)$p.value
+    
+    rp[1] = substitute(expression(italic(R^2) == R.Rval), 
+                       list(R.Rval = format(R.Rval,dig=3)))[2]
+    rp[2] = substitute(expression(italic(F) == R.Fval), 
+                       list(R.Fval = format(R.Fval,dig=3)))[2]
+
+    rp[3] = substitute(expression(italic(p) == R.pval), 
+                       list(R.pval = format(R.pval, digits = 3)))[2]
+    legend('topleft', legend = rp, bty = 'n')
+    
+    # abline(RES, col = "red",lwd = 2)#, lty = "dashed")# need to add a trend line 
+    
+  }
+  init <- RES$fitted.values[1]
+  fin <- RES$fitted.values[end(RES$fitted.values)[1]]
+  change <- fin - init
+  return(structure(list(Method = "RESTREND", 
+                        VPR = VPR.fit, TSS.RESTREND = RES, total.change = change), class = "RESTREND.Object"))
+}
+
 
 #CTSR.VI <- stdRESTREND.CTSR$cts.NDVI
 #CTSR.RF <- stdRESTREND.CTSR$cts.precip
 a<- BFAST.RESID(stdRESTREND.CTSR$cts.NDVI, stdRESTREND.CTSR$cts.precip, print=TRUE, plot=TRUE)
 print(a)
 
-#anu.VI <- stdRESTREND$max.NDVI
-#acu.RF <- stdRESTREND$acc.precip
-#VI.index <- stdRESTREND$index
+#fin functions use class(a) to determine if its numeric or logical
 
-res <- RESTREND(stdRESTREND$max.NDVI, stdRESTREND$acc.precip, stdRESTREND$index, sig=0.05, breakpoint=FALSE, print=TRUE, plot=TRUE) 
+se<- BFAST.RESID(segRESTREND.CTSR$cts.NDVI, segRESTREND.CTSR$cts.precip, print=TRUE, plot=TRUE)
+print(se)
+#CHOW <- function(anu.VI, acu.RF, VI.index, se, sig=0.05, print=FALSE)
+
+#for testing the chow test
+anu.VI <- segRESTREND$max.NDVI
+acu.RF <- segRESTREND$acc.precip
+VI.index <- segRESTREND$index
+
+se.chow <- CHOW(anu.VI, acu.RF, VI.index, se, sig=0.05, print=TRUE)
+breakpoint = as.integer(se.chow$bp.summary[2])
+se.RES <- seg.RESTREND(anu.VI, acu.RF, VI.index, breakpoint,  sig=0.05, print=TRUE, plot=TRUE)
+
+anu.VI <- stdRESTREND$max.NDVI
+acu.RF <- stdRESTREND$acc.precip
+VI.index <- stdRESTREND$index
+res <- RESTREND(anu.VI, acu.RF, VI.index, sig=0.05, print=TRUE, plot=TRUE) 
+
+res <- RESTREND(stdRESTREND$max.NDVI, stdRESTREND$acc.precip, stdRESTREND$index, sig=0.05, print=TRUE, plot=TRUE) 
 
