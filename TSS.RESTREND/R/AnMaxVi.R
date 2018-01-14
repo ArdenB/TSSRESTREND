39d59041b06944ac8e5734e7033cb7ab85fc4b9e
Arden <arden.burrell@student.unsw.edu.au>
2018-01-14 13:25:25 +1100

    Updated documentation on the AnMaxVI script

--- TSS.RESTREND/R/AnMaxVi.R
+++ TSS.RESTREND/R/AnMaxVi.R
@@ -21,58 +21,78 @@
 #' print(anmax)
 
 
-AnMaxVI <- function(CTSR.VI){
-  if (class(CTSR.VI) != "ts")
+AnMaxVI <- function(CTSR.VI) {
+  # ==============================================================================================
+  # ========== Sanity check the input data ==========
+  if (class(CTSR.VI) != "ts") {
     stop("CTSR.VI Not a time series object")
-  #Work out the start and end dates
+  }
+  # ==============================================================================================
+  # =========== Organise the data and key variables ==========
+  # +++++ Work out the start and end dates +++++
   sty <- start(CTSR.VI)[1]
-  stm <-  start(CTSR.VI)[2]
+  stm <- start(CTSR.VI)[2]
   eny <- end(CTSR.VI)[1]
   enm <- end(CTSR.VI)[2]
-  #Sort by year so its easier to find the annual max values
-  m<- matrix(nrow=(eny-sty+1), ncol=12)
-  rownames(m)<- c(sty:eny)
-  colnames(m)<- c(month.abb[1:12])
+  # ++++++ Create a blank matrix+++++
+  # matrix is sorted by year so its easier to find the annual max values
+  m <- matrix(nrow = (eny - sty + 1), ncol = 12)
+  rownames(m) <- c(sty:eny)
+  colnames(m) <- c(month.abb[1:12])
   index <- 1
-  for (yr in sty:eny){
-    # m[toString(yr), 13:14] = 0
-    # print(yr)
-    for (mon in 1:12){
-      if (yr==sty & mon<stm){
+  # +++++  loop over each year To set the names +++++
+  for (yr in sty:eny) {
+    for (mon in 1:12) {
+      if (yr == sty & mon < stm) {# Ignore months before the start
         m[toString(yr), month.abb[mon]] = NaN
-      }else if (yr==eny & mon>enm){
+      }else if (yr == eny & mon > enm) { # Ignore months After the end of the data
         m[toString(yr), month.abb[mon]] = NaN
       }else{
         m[toString(yr), month.abb[mon]] = CTSR.VI[index]
       }
-      index <- (index+1)
+      index <- (index + 1)
     }
   }
-  anmax.ts <- ts(apply(m,1,max, na.rm=TRUE), start=sty, frequency = 1)
-  whmax.ts <- ts(apply(m,1,which.max), start=sty, frequency = 1)
-  index.ts <- ts(c(sty:eny), start=sty, frequency = 1)
-  df <- data.frame( Max=anmax.ts, Max.month=whmax.ts, index = index.ts)
-  for (row in 1:(dim(df)[1]-1)){
-    if ((df$Max.month[row] == 11 || df$Max.month[row] == 12) && (df$Max.month[row+1]==1 || df$Max.month[row+1]==2)){
-      # print(df[row,])
-      # print(row)
-      test <- c(m[row, 11:12], m[row+1, 1:2])
-      # print(test)
-      df$Max.month[row] = which.max(test)+10
-      df$Max[row] = max(test, na.rm=TRUE)
-      try({
-        nxtyr <- m[row+2, 2:12]
-        df$Max.month[row+1] = which.max(nxtyr)+2
-        df$Max[row+1] = max(nxtyr)
+  # ==============================================================================================
+  # =========== Work out the seasonal max NDVI values ==========
+
+  # =========== Get the annual max NDVI values ==========
+  # +++++ build time series of max values, max months and max index +++++
+  anmax.ts <- ts(apply(m, 1, max, na.rm = TRUE), start = sty, frequency = 1)
+  whmax.ts <- ts(apply(m, 1, which.max), start = sty, frequency = 1)
+  index.ts <- ts(c(sty:eny), start = sty, frequency = 1)
+  # +++++ add these ts' to a dataframe
+  df <- data.frame( Max = anmax.ts, Max.month = whmax.ts, index = index.ts)
+
+  # ========== Test seasonality ============
+  # Loop over each year
+  for (row in 1:(dim(df)[1] - 1)) {
+    # Check and see of the max values are occuring in nov and december
+    if (
+      (df$Max.month[row] == 11 || df$Max.month[row] == 12) &&
+      (df$Max.month[row + 1] == 1 || df$Max.month[row + 1] == 2)
+      ) {# Value occurs in the NDJF period
+      # Add Jan anf Feb data and re check the max
+      test <- c(m[row, 11:12], m[row + 1, 1:2])
+      df$Max.month[row] = which.max(test) + 10
+      df$Max[row] = max(test, na.rm = TRUE)
+      # exclude jan and feb for the subsuquent years calculation
+      try({# Should only fail on the last year, this is to catch that
+        nxtyr <- m[row + 2, 2:12]
+        df$Max.month[row + 1] = which.max(nxtyr) + 2
+        df$Max[row + 1] = max(nxtyr)
       }, silent = TRUE)
 
     }
   }
-  df$index <- ((df$index-sty)*12+df$Max.month)
-  #need to test this
-  if (stm != 1){
-    df$index <- (df$index-(stm-1))
-    print("this feature requires further testing")
+  df$index <- ((df$index - sty) * 12 + df$Max.month)
+  if (stm != 1) {# the first month of data is not january
+    df$index <- (df$index - (stm - 1))
+    warning(
+      "TSS-RESTREND should work with any start month.
+      However this has not been tested. Additional
+      caution should be excercised when considering
+      the results ")
   }
   return(df)
 }
